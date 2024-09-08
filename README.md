c9f46fed008c99468338914491107fb2b760d4f7
JayaKrishna <jayakrishna.thupili@gmail.com>
2024-09-08 18:17:51 -0500

    Update SMS gateway options

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # SMS-Connect-Pro
 
 SMS-Connect Pro: AI-Powered Contact &amp; Messaging Platform
+
+### SMS Gateways
+
+- https://docs.aws.amazon.com/sns/latest/dg/channels-sms-originating-identities.html
+- https://aws.amazon.com/sns/sms-pricing/
+- https://www.twilio.com/en-us/lp/sms-signup-api-for-messaging-at-scale
+- https://www.sinch.com/products/apis/messaging/sms/
+- https://telnyx.com/phone-numbers/florida
--- sms-connect-pro-fe/src/App.js
+++ sms-connect-pro-fe/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './components/Home';
 import Contacts from './components/Contacts';
 import SendMessage from './components/SendMessage';
@@ -13,10 +13,13 @@ function App() {
         <Sidebar />
         <div id="page-content-wrapper">
           <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
             <Route path="/home" element={<Home />} />
             <Route path="/contacts" element={<Contacts />} />
             <Route path="/sendmessage" element={<SendMessage />} />
             <Route path="/settings" element={<Settings />} />
+            {/* 404 fallback route */}
+            <Route path="*" element={<h1>404 - Page Not Found</h1>} />
           </Routes>
         </div>
       </div>
